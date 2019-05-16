930f453eadde2b50ec16c7b2a5ed3451e974f26f
leenorris <lee.norris@philips.com>
2019-05-16 10:21:17 -0400

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as functions from 'firebase-functions';
-
+import {cpus} from 'os';
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send("Hello from Firebase!" + cpus().map(cpu => JSON.stringify(cpu)).join(' '));
 });
